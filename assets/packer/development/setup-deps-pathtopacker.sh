3bc4ae3a4e7d7d5e5dbcb86e0329d27adfea6168
sonyahash <85360714+sonyahash@users.noreply.github.com>
2022-06-30 13:23:23 -0500

    Update setup-deps-pathtopacker.sh

--- assets/packer/development/setup-deps-pathtopacker.sh
+++ assets/packer/development/setup-deps-pathtopacker.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
-set -e
+!/bin/bash
+# Script to deploy a very simple web application.
+# The web app has a customizable image and some text.
 
-export DEBIAN_FRONTEND=noninteractive
-export TZ=America/Toronto
+cat << EOM > /var/www/html/index.html
+<html>
+  <head><title>Meow!</title></head>
+  <body>
+  <div style="width:800px;margin: 0 auto">
+  <!-- BEGIN -->
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><h2>Meow World!</h2></center>
+  Welcome to ${PREFIX}'s app. Meow meow meow. 
+  <!-- END -->
+  </div>
+  </body>
+</html>
+EOM
 
-# Install necessary dependencies
-sudo apt-get update
-sudo apt-get -y install git docker.io unzip auditd
-
-# Install latest version of docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Get HashiCups config
-git clone https://github.com/hashicorp-demoapp/pathtopacker-setups
-cd pathtopacker-setups/docker-compose-deployment
-git checkout server
-
-# Changing the default entry point password for DB to avoid
-# default password in config file.
- RANDOM_PASSWORD=$(openssl rand -hex 20)
- sudo sed -i "s/=password/=$RANDOM_PASSWORD/g" docker-compose.yaml
- sudo sed -i "s/=password/=$RANDOM_PASSWORD/g" conf.json
- unset RANDOM_PASSWORD
-
-# Start HashiCups in background
-sudo docker-compose up -d
+echo "Script complete."
